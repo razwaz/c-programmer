55f33ab472838bb22791c1c848042b010de21579
razwaz <andpdud@gmail.com>
2021-10-31 13:45:22 +0100

    update

--- Lektion9/roll_dice.c
+++ Lektion9/roll_dice.c
@@ -4,34 +4,31 @@
 
 #define DIE_MAX_EYES 6
 
-void roll_multiple_dice(int *dice_count, int *dice[(int)dice_count]);
+int roll_dice();
 
 
 int main(void) {
   int i;
   int dice_count = 5;
-
-  scanf("%d\n", &dice_count);
-  int dice[dice_count];
-
-
-
-
+  int loop;
   srand(time(NULL));  // Seeding the random number generator.
                       // Do this only once!
-/*
-  for(i = 1; i <= 10; i++)
-    printf("The die shows: %d\n", roll_die());
-    */
-    roll_multiple_dice(&dice_count, &dice[(int)&dice_count]);
+
+  scanf(" %d\n", &dice_count);
+  int dice[dice_count];
+
+  for (loop = 0; loop < dice_count; loop++) {
+    dice[loop] = roll_dice();
+    printf(" %lf\n", dice[loop]);
+  }
+
+  printf ("%lf\n", dice[1]);
 
   return 0;
 }
 
-void roll_multiple_dice(int *dice_count, int *dice[(int)*dice_count]){
-  int loop;
-
-  for (loop = 0; loop < *dice_count; loop++) {
-    *dice[loop] = (rand() % DIE_MAX_EYES) + 1;
-    }
+int roll_dice(){
+  int dice;
+  dice = (rand() % DIE_MAX_EYES) + 1;
+  return dice;
 }
