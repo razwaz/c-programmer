34bb3c8bfab7753578210718f322bd97504097b7
razwaz <andpdud@gmail.com>
2021-11-24 15:22:52 +0100

    Pseudo code translated (but wrong)

--- DTG/W3/mergeSort.c
+++ DTG/W3/mergeSort.c
@@ -20,11 +20,60 @@ int main(void){
 }
 
 void Merge(int L[], int start, int end, int mid){
-/*Here goes your code*/
+  /*Here goes your code*/
+  int i = 0;
+  int j = 0;
+  int k;
+  printf("Start: %d, Mid: %d, End: %d\n", start, mid, end);
+  int L1[end];
+  int L2[end];
+  for (i = start; i <= mid; i++) {
+    L1[i-start] = L[i];
+  }
+  for (i = mid; i <= end; i++) {
+    L2[i-mid] = L[i+1];
+  }
+  i = 0;
+  j = 0;
+
+  do {
+    if (L1[i] <= L2[j]) {
+      L[start+i+j] = L1[i];
+      i++;
+    } else {
+      L[start+i+j] = L2[j];
+      j++;
+    }
+  } while (i < (mid - start) && j < (end - mid));
+
+  if (i = mid - start+1) {
+    for (k = j; k < end - mid - 1; k++){
+      L[start+i+k] = L2[k];
+    }
+  } else {
+    for (k = i; k < mid - start; k++) {
+      L[start+j+k] = L1[k];
+    }
+  }
+
+
+
+  printf("L1: ");
+  printList(L1, mid);
+  printf("L2: ");
+  printList(L2, end);
+
 }
 
 void MergeSort(int L[], int start, int end){
+  int m = 0;
   /*Here goes your code as well*/
+  if (start < end) {
+    m = (end+start)/2;
+    MergeSort(L, start, m);
+    MergeSort(L, m+1, end);
+    Merge(L, start, end, m);
+  }
 }
 
 void printList(int L[], int size){
