b38961ecd894df7494c54f50963e08ea1bce3a27
razwaz <andpdud@gmail.com>
2021-11-26 11:23:52 +0100

    Pseudo code translated (correctly)

--- DTG/W3/mergeSort.c
+++ DTG/W3/mergeSort.c
@@ -1,3 +1,9 @@
+/* Merge sort program made from the pseudo code from DTG workshop 3.
+ * The program is a great example of recursive functions.
+ * The program has a bunch of printf's to show how the program works and
+ * to make debugging much less of a hassle.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,6 +13,7 @@ void printList(int L[], int size);
 
 int main(void){
   int L[] = { 5, 3, 8, 1, 6, 10, 7, 2, 4, 9 };
+  printf("L[0]=%d\n",L[0]);
   int L_size = sizeof(L) / sizeof(L[0]);
 
   printf("Given list \n");
@@ -14,25 +21,36 @@ int main(void){
 
   MergeSort(L, 0, L_size - 1);
 
+  printf("-------------------------------------\n");
   printf("The sorted list \n");
   printList(L, L_size);
+  printf("-------------------------------------\n");
   return 0;
 }
 
 void Merge(int L[], int start, int end, int mid){
   /*Here goes your code*/
-  int i = 0;
-  int j = 0;
-  int k;
+  int i, j, k;
+  printf("-------------------------------------\n");
   printf("Start: %d, Mid: %d, End: %d\n", start, mid, end);
-  int L1[end];
-  int L2[end];
-  for (i = start; i <= mid; i++) {
-    L1[i-start] = L[i];
+
+  int L1[mid-start+1];
+  int L2[end-mid];
+
+  for (i = 0; i < mid-start+1; i++) {
+    L1[i] = L[start+i];
   }
-  for (i = mid; i <= end; i++) {
-    L2[i-mid] = L[i+1];
+  for (i = 0; i <= end-mid; i++) {
+    L2[i] = L[mid+i+1];
   }
+
+  printf("L: ");
+  printList(L, 10);
+  printf("L1: ");
+  printList(L1, mid-start+1);
+  printf("L2: ");
+  printList(L2, end-mid);
+
   i = 0;
   j = 0;
 
@@ -40,34 +58,27 @@ void Merge(int L[], int start, int end, int mid){
     if (L1[i] <= L2[j]) {
       L[start+i+j] = L1[i];
       i++;
+
     } else {
       L[start+i+j] = L2[j];
       j++;
     }
-  } while (i < (mid - start) && j < (end - mid));
+  } while (i < (mid - start + 1) && j < (end - mid));
 
-  if (i = mid - start+1) {
-    for (k = j; k < end - mid - 1; k++){
+  if (i == (mid - start+1)) {
+    for (k = j; k < (end - mid); k++){
       L[start+i+k] = L2[k];
     }
   } else {
-    for (k = i; k < mid - start; k++) {
+    for (k = i; k < (mid - start + 1); k++) {
       L[start+j+k] = L1[k];
     }
   }
-
-
-
-  printf("L1: ");
-  printList(L1, mid);
-  printf("L2: ");
-  printList(L2, end);
-
 }
 
 void MergeSort(int L[], int start, int end){
-  int m = 0;
   /*Here goes your code as well*/
+  int m;
   if (start < end) {
     m = (end+start)/2;
     MergeSort(L, start, m);
@@ -79,6 +90,6 @@ void MergeSort(int L[], int start, int end){
 void printList(int L[], int size){
   int i;
   for (i = 0; i < size; i++)
-    printf("%d ", L[i]);
+  printf("%d ", L[i]);
   printf("\n");
 }
